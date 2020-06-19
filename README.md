73f7a5506b22bfe64c4b840c253309de70b3ff83
Daniel Joos <daniel@joosweb.de>
2020-06-19 22:54:23 +0200

    Adds README, LICENSE, build

---
+++ README.md
@@ -0,0 +1,14 @@
+# sundae-rounded
+
+Simple rounded sans-serif font that I want to use for keycap legends.
+
+![preview](preview.png)
+
+## Remarks
+
+* At the moment the font contains only Latin uppercase letters.
+* The font does not define proper letter spacing or ligatures as its intended use case is the design of keycap legends.
+
+## License
+
+ SIL OFL 1.1 License (See file LICENSE).
---
+++ build.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+const svg2ttf = require('svg2ttf');
+const SVGIcons2SVGFontStream = require('svgicons2svgfont');
+
+const fontName = 'sundae-rounded';
+const metadata = 'Copyright (c) 2020 - Daniel Joos';
+const ttfFileName = 'sundae-rounded.ttf';
+
+async function createSvgFont() {
+    const fontStream = new SVGIcons2SVGFontStream({fontName, metadata});
+
+    const buffers = [];
+    const pr = new Promise((resolve) => {
+        fontStream.on('data', (chunk) => buffers.push(chunk));
+        fontStream.on('end', () => {
+            const result = Buffer.concat(buffers).toString();
+            resolve(result);
+        });
+    });
+
+    const files = await fs.promises.readdir('glyphs');
+    for (const file of files) {
+        const glyph = fs.createReadStream(`glyphs/${file}`);
+        glyph.metadata = {
+            unicode: [file.charAt(0)],
+            name: file.charAt(0),
+        };
+        fontStream.write(glyph);
+    }
+    fontStream.end();
+
+    return pr;
+}
+
+async function preview() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`file:///${__dirname}/preview.html`);
+    const previewArea = await page.$('#preview');
+    await previewArea.screenshot({ path: 'preview.png', omitBackground: true });
+    await browser.close();
+}
+
+(async () => {
+    // Create TTF font
+    const svgFont = await createSvgFont();
+    var ttf = svg2ttf(svgFont, {});
+    await fs.promises.writeFile(ttfFileName, new Buffer(ttf.buffer));
+
+    // Render Preview
+    await preview();
+})();
